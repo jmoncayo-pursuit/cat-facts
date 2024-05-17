bdde47ffdf9c6eb8361ec11a2341640ed353ecc7
jmoncayo-pursuit <jmoncayo@pursuit.org>
2024-05-17 13:32:42 -0400

    Initial commit

--- README.md
+++ README.md
@@ -4,3 +4,13 @@ This template provides a minimal setup to get React working
 
 
 # React-Empty-Template
+[x] 1. start a new vite project
+[x] 2. set some text on the home page
+[x] 3. fetch a cat fact from the
+catfact api on load
+// URL https://catfact.ninja/
+// 4. add a button that fetches a
+new fact and appends it to the current facts
+// 5. bonus: add a button on each
+fact that removes it from state when it is clicked
+
--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,39 @@
-import { useState } from 'react'
-import './App.css'
+import { useState, useEffect } from 'react';
+import './App.css';
 
 function App() {
+  const [catFacts, setCatFacts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://catfact.ninja/fact')
+      .then(response => response.json())
+      .then(data => setCatFacts([data.fact]));
+  }, []);
+
+  const fetchNewFact = () => {
+    fetch('https://catfact.ninja/fact')
+      .then(response => response.json())
+      .then(data => setCatFacts([...catFacts, data.fact]));
+  };
+
+  const removeFact = (index) => {
+    setCatFacts(catFacts.filter((_, i) => i !== index));
+  };
 
   return (
-    <>
-      
-    </>
-  )
+    <div className="App">
+      <h1>Cat Facts</h1>
+      <ul>
+        {catFacts.map((fact, index) => (
+          <li key={index}>
+            {fact}
+            <button onClick={() => removeFact(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+      <button onClick={fetchNewFact}>Get New Fact</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
